9e4f7eff11cbbb9aa63e91ddcf5e4fc014ff4f58
Daniel Delgado <delgado.jdds@gmail.com>
2018-05-12 16:39:00 -0600

    Revert "Merge branch 'varInitialized'"
    This reverts commit b71ac819fc50da96c1e1b3f63050dcbf111f9e91.

--- Triangle/ContextualAnalyzer/Checker.java
+++ Triangle/ContextualAnalyzer/Checker.java
@@ -902,13 +902,7 @@ public final class Checker implements Visitor {
 
     @Override
     public Object visitVarInitialized(VarInitialized ast, Object o) {
-        TypeDenoter eType = (TypeDenoter) ast.E.visit(this,null);
-        ast.I.type = eType;
-        idTable.enter (ast.I.spelling, ast);
-        if (ast.duplicated)
-          reporter.reportError ("identifier \"%\" already declared", ast.I.spelling, ast.position);
-
-        return null;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
