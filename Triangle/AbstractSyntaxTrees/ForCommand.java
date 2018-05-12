7c6868b40aa9a64f53187bc19153a689fb274e09
fabriales14 <fabriales14@gmail.com>
2018-05-12 12:38:15 -0600

    Files for contextual analyzer

--- Triangle/AbstractSyntaxTrees/ForCommand.java
+++ Triangle/AbstractSyntaxTrees/ForCommand.java
@@ -1,7 +1,8 @@
-// Added for the Proyect Number 1.
-// Syntax analyzer.
-// 04/25/2018
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Triangle.AbstractSyntaxTrees;
 
 import Triangle.SyntacticAnalyzer.SourcePosition;
